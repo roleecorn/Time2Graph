48e74a60c090159f4b5f97f0d913a9fd9d2eb669
roleecorn <roleecorn@gmail.com>
2023-06-19 12:00:44 +0800

    result_origin_method

--- script_run.py
+++ script_run.py
@@ -3,9 +3,9 @@ import concurrent.futures
 
 # 定義一個函數來運行命令
 def run_command(i):
-    os.system('python scripts/run.py --dataset ucr-Power{}   --kernel dts --seg_length 60 --K 30 --gpu_enable'.format(i))
+    os.system('python scripts/run.py --dataset ucr-Power{}   --kernel dts --seg_length 60 --K 30'.format(i))
 
 # 創建一個 ThreadPoolExecutor
-with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
     # 使用 executor.map 函數來並行地運行指令
-    executor.map(run_command, range(100))
+    executor.map(run_command, range(100,110))
